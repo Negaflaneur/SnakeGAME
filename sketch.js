3c3afafd94efb8d11cbfadefd76e92dd54661714
Negative11119 <55886337+Negative11119@users.noreply.github.com>
2019-09-27 18:01:32 +0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,63 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+let img;
+
+function preload(){
+img = loadImage('snake.jpg');
+}
+
+function setup() {
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(10);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+  
+}
+
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+  	snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+  	snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+  	snake.setDir(0, -1);
+  } else if (key == ' ') {
+  	snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  // background(220);
+  image(img, 0, 0, width, height);
+  if (snake.eat(food)) {
+     foodLocation();
+  }
+  snake.update();
+  snake.show();
+  
+  
+  if (snake.endGame()) {
+  	print("Game over");
+    background(255, 0, 0);
+    noLoop();
+  }
+  
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
